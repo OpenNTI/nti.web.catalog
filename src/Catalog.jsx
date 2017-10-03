b3d31d130d80c2c4ffe1b1eb077af3c8fd3422ac
Nguyen Ngoc Phuong Thao <thaonguyen@goappable.com>
2017-10-03 09:11:15 +0700

    upload Css for Catalog.

--- src/Catalog.jsx
+++ src/Catalog.jsx
@@ -9,68 +9,47 @@ export default function Catalog ({items}) {
 		<div className="container">
 			<main>
 				<div className="wrapper">
-					<section className="slider"> 
-						<div className="detail-slider">
-							<div className="slider-block item1">
-								<div className="content-slider">
-									<h3 className="title-slider">Fundamentals of Engineering Statistical Analysis</h3>
-									<p className="detail-txt">This course provides fundamental concepts in probability and statistical inference, with application to engineering contexts.</p>
-									<div className="statistic">
-										<ul>
-											<li> 
-												<p className="date">Starts December 30  </p>
-											</li>
-											<li>
-												<p className="visiter">•  4,321 Learners</p>
-											</li>
-											<li> 
-												<p className="spot">20 Spots Left</p>
-											</li>
-										</ul>
-									</div>
-								</div>
+					<section className="slider">
+						<div className="slider-block item1">
+							<div className="content-slider">
+								<h3 className="title-slider">Fundamentals of Engineering Statistical Analysis</h3>
+								<p className="detail-txt">This course provides fundamental concepts in probability and statistical inference, with application to engineering contexts.</p>
+								<div className="statistic">
+									<ul>
+										<li>
+											<p className="date">Starts December 30  </p>
+										</li>
+										<li>
+											<p className="visiter">• &nbsp; &nbsp; 4,321 Learners</p>
+										</li>
+										<li>
+											<p className="spot">20 Spots Left</p>
+										</li>
+									</ul>
+								</div><img alt="slider" src="https://janux.ou.edu/masters/dsa/resources/images/video-cs5013.png" className="img-content"/>
 							</div>
-							<div className="slider-block item2">
-								<div className="content-slider">
-									<h3 className="title-slider">Fundamentals of Engineering Statistical Analysis</h3>
-									<p className="detail-txt">This course provides fundamental concepts in probability and statistical inference, with application to engineering contexts.</p>
-									<div className="statistic">
-										<ul>
-											<li> 
-												<p className="date">Starts December 1  </p>
-											</li>
-											<li>
-												<p className="visiter">•  4,321 Learners</p>
-											</li>
-											<li> 
-												<p className="spot">200 Spots Left          </p>
-											</li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							<button className="arrow-left">prev</button>
-							<button className="arrow-right"> next</button>
 						</div>
+						<button className="arrow-left"/>
+						<button className="arrow-right"/>
 					</section>
 					<section className="content-catalog">
-						<div className="side-bar-right">
+						<div className="side-bar-left">
 							<div className="item popular-block">
 								<p className="title-sidebar">POPULAR COURSES</p>
 								<ol>
-									<li> 
+									<li><span>1</span>
 										<p>Law and Jusice</p>
 									</li>
-									<li> 
+									<li><span>2</span>
 										<p>Chemistry of Beer</p>
 									</li>
-									<li> 
+									<li><span>3</span>
 										<p>Gateway to College Learning  </p>
 									</li>
-									<li> 
+									<li><span>4</span>
 										<p>Physical Geology for Scien …</p>
 									</li>
-									<li> 
+									<li><span>5</span>
 										<p>Data Analyitcs  </p>
 									</li>
 								</ol>
@@ -78,59 +57,71 @@ export default function Catalog ({items}) {
 							<div className="item tag-block">
 								<p className="title-sidebar">BROWSE BY TAG</p>
 								<ul>
-									<li> 
+									<li>
 										<p>Free Courses</p>
 									</li>
-									<li> 
+									<li>
 										<p>Physics</p>
 									</li>
-									<li> 
+									<li>
 										<p>Computer Science  </p>
 									</li>
-									<li> 
+									<li>
 										<p>Health</p>
 									</li>
-									<li> 
+									<li>
 										<p>Economics </p>
 									</li>
 								</ul>
 							</div>
 							<div className="item redeem-block">
-								<p>Redeem Code       </p>
+								<ul>
+									<li>
+										<p>Redeem Code       </p>
+									</li>
+								</ul>
 							</div>
 						</div>
 						<div className="course-card">
-							<div className="course-block"><img alt="course" src="assets/images/course1.png"/>
+							<div className="course-block">
+								<figure><img alt="course" src="images/course1.png"/></figure>
 								<div className="info-course"><span>STATS 3201</span>
 									<h3>INTRODUCTION TO WATER</h3><a href="#">R. DOUGLAS ELMORE, PHD</a>
 								</div>
 							</div>
-							<div className="course-block"><img alt="course" src="assets/images/course2.png"/>
+							<div className="course-block">
+								<figure><img alt="course" src="images/course2.png"/></figure>
 								<div className="info-course"><span>BIOL 2124</span>
 									<h3>HUMAN PHYSIOLOGY</h3><a href="#">R. DOUGLAS ELMORE, PHD</a>
 								</div>
 							</div>
-							<div className="course-block"><img alt="course" src="assets/images/course3.png"/>
+							<div className="course-block">
+								<figure><img alt="course" src="images/course3.png"/></figure>
 								<div className="info-course"><span>ECON 2843</span>
 									<h3>ELEMANTARY BUSINESS STATISTICS</h3><a href="#">R. DOUGLAS ELMORE, PHD        </a>
 								</div>
-								<div className="enroll"><a>ENROLLED</a></div>
+								<div className="stamp"><a className="enroll">ENROLLED</a></div>
 							</div>
-							<div className="course-block"><img alt="course" src="assets/images/course4.png"/>
+						</div>
+						<div className="course-card">
+							<div className="course-block">
+								<figure><img alt="course" src="images/course4.png"/></figure>
 								<div className="info-course"><span>UCOL 1002</span>
 									<h3>Gateway to College Learning</h3><a href="#">LILEAN MILLER, M.ED.</a>
 								</div>
 							</div>
-							<div className="course-block"><img alt="course" src="assets/images/course5.png"/>
+							<div className="course-block">
+								<figure><img alt="course" src="images/course5.png"/></figure>
 								<div className="info-course"><span>SS 001</span>
 									<h3>GARY ENGLAND’S TORNADO ALLEY</h3><a href="#">R. DOUGLAS ELMORE, PHD</a>
 								</div>
 							</div>
-							<div className="course-block"><img alt="course" src="assets/images/course3.png"/>
+							<div className="course-block">
+								<figure><img alt="course" src="images/course3.png"/></figure>
 								<div className="info-course"><span>ECON 2843</span>
 									<h3>ELEMANTARY BUSINESS STATISTICS</h3><a href="#">R. DOUGLAS ELMORE, PHD        </a>
 								</div>
-								<div className="enroll"><a>STARTS NOV. 23    </a></div>
+								<div className="stamp"><a className="start">STARTS NOV. 23    </a></div>
 							</div>
 						</div>
 					</section>
