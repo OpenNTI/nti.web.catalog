0543f170e56e5ada27e78359f26a1c4788053573
Son Nguyen <sonnguyen@goappable.com>
2017-10-20 14:31:24 +0700

    open course detail when click course panel

--- src/components/grid-card/card/Card.jsx
+++ src/components/grid-card/card/Card.jsx
@@ -1,35 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropType from 'prop-types';
 import {encodeForURI} from 'nti-lib-ntiids';
 
-CourseCard.propTypes = {
-	imgUrl: PropType.string,
-	courseId: PropType.string,
-	courseTitle: PropType.string,
-	author: PropType.string,
-	status: PropType.shape ({
-		OpenEnrollment: PropType.object
-	}),
-	ntiid: PropType.string
-};
+export default class CourseCard extends Component {
+	static propTypes = {
+		imgUrl: PropType.string,
+		courseId: PropType.string,
+		courseTitle: PropType.string,
+		author: PropType.string,
+		status: PropType.shape ({
+			OpenEnrollment: PropType.object
+		}),
+		ntiid: PropType.string
+	}
 
-export default function CourseCard (data) {
-	const status = data.status && data.status.OpenEnrollment &&
-		data.status.OpenEnrollment.IsEnrolled;
+	showDetail = (link) => () => {
+		console.log ('detail: ', link);
+		const detailLink = `./object/${encodeForURI (this.props.ntiid)}`;
+		window.open (detailLink, '_self');
+	}
 
-	return (
-		<div className="course-block">
-			<a href={`./object/${encodeForURI(data.ntiid)}`}>
+	render () {
+		const status = this.props.status && this.props.status.OpenEnrollment &&
+			this.props.status.OpenEnrollment.IsEnrolled;
+
+		return (
+			<div className="course-block" onClick={this.showDetail(this.props.ntiid)}>
 				<figure>
-					<img alt="course" src={data.imgUrl}/>
+					<img alt="course" src={this.props.imgUrl}/>
 				</figure>
-			</a>
-			<div className="info-course"><span>{data.courseId}</span>
-				<h3>{data.courseTitle}</h3>
-				<a href="#">{data.author}</a>
+				<div className="info-course"><span>{this.props.courseId}</span>
+					<h3>{this.props.courseTitle}</h3>
+					<a href="#">{this.props.author}</a>
+				</div>
+				{status && (
+					<div className="stamp"><a className="enroll">ENROLLED</a></div>)}
 			</div>
-			{status && (
-				<div className="stamp"><a className="enroll">ENROLLED</a></div>)}
-		</div>
-	);
+		);
+	}
 }
