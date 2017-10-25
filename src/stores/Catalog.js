331979885f3d1edd3d8af1c229a746f0517d4a46
Son Nguyen <sonnguyen@goappable.com>
2017-10-25 16:58:43 +0700

    get api link

--- src/stores/Catalog.js
+++ src/stores/Catalog.js
@@ -15,13 +15,13 @@ export default class CatalogStore extends EventEmitter {
 
 	async load () {
 		const {Items: courses, Links: links} = await this.service.get (this.href);
-		const {Items: popular} = await this.service.get (links[0].href);
+		const {Items: popular} = await this.service.get (getAPILink (links, 'Popular'));
 
 		this.courses = courses;
 		this.popular = popular;
 
 		this.emit (CHANGE, {type: 'courses'});
-		this.emit(CHANGE, {type: 'popular'});
+		this.emit (CHANGE, {type: 'popular'});
 	}
 
 
@@ -37,4 +37,14 @@ export default class CatalogStore extends EventEmitter {
 	removeChangeListener (fn) {
 		this.removeListener (CHANGE, fn);
 	}
+}
+
+function getAPILink (data, rel) {
+	let apiLink = '';
+	data.map ((api) => {
+		if (api.rel === rel) {
+			apiLink = api.href;
+		}
+	});
+	return apiLink;
 }
