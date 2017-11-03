ffd8d96811bb26777d19484e139035602dcd1509
Son Nguyen <sonnguyen@goappable.com>
2017-11-03 16:19:25 +0700

    using presentation.AssetBackground component to show image on carousel

--- src/components/carousel/carousel-item/CarouselItem.jsx
+++ src/components/carousel/carousel-item/CarouselItem.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {DateTime} from 'nti-web-commons';
+import {Presentation, DateTime} from 'nti-web-commons';
 
 
 CarouselItem.propTypes = {
-	backgroundUrl: PropTypes.string,
-	detailImage: PropTypes.string,
-	title: PropTypes.string,
-	description: PropTypes.string,
-	startDate: PropTypes.instanceOf(Date),
+	data: PropTypes.object
 };
 
 export default function CarouselItem (props) {
-	return (
-		<div className="carousel-block" style={{'backgroundImage': 'url(' + props.backgroundUrl + ')'}}>
-			<div className="content-carousel">
-				<h3 className="title-carousel">{props.title}</h3>
-				<p className="detail-txt">{props.description}</p>
-				<div className="statistic">
-					<ul>
-						<li>
-							<p className="date">Starts <DateTime date={props.startDate}/></p>
-						</li>
-					</ul>
-				</div><img alt="carousel" src={props.detailImage} className="img-content"/>
-			</div>
+	return(
+		<div>
+			<Presentation.AssetBackground type="background" contentPackage={props.data}>
+				<div className="carousel-block">
+					<div className="content-carousel">
+						<h3 className="title-carousel">{props.data.title}</h3>
+						<p className="detail-txt">{props.data.description}</p>
+						<div className="statistic">
+							<ul>
+								<li>
+									<p className="date">Starts <DateTime date={props.data.StartDate}/></p>
+								</li>
+							</ul>
+						</div>
+						<Presentation.Asset contentPackage={props.data} propName="src" type="landing">
+							<img className="img-content" />
+						</Presentation.Asset>
+					</div>
+				</div>
+			</Presentation.AssetBackground>
 		</div>
 	);
 }
