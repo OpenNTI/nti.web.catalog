623acb93f5cb6dfca69767fcfbfa55d0fc73263b
Son Nguyen <sonnguyen@goappable.com>
2017-11-06 16:53:22 +0700

    update redeem

---
+++ src/components/redeem/Redeem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Redeem extends React.Component {
+	render () {
+		return (
+			<div className="redeem-class">
+				<p>redeem</p>
+			</div>
+		);
+	}
+}
---
+++ src/components/search/Search.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import * as Actions from '../../Actions';
+
+export default class Search extends React.Component {
+	static propTypes = {
+		data: PropTypes.object
+	}
+
+	viewAllCourse = () => {
+		Actions.viewAllCourses();
+	}
+
+	render () {
+		if (this.props.data && this.props.data.term) {
+			let term = '"' + this.props.data.term + '"';
+			return (
+				<div className="search-result">
+					<a onClick={this.viewAllCourse}>View all courses</a>
+					<p>Result for {term}</p>
+				</div>
+			);
+		}
+
+		return null;
+	}
+}
