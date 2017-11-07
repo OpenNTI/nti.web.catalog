307f735145ac96c48f0e21c01554ccf7d49e09a7
PhucHongTran <31498749+PhucHongTran@users.noreply.github.com>
2017-11-07 17:04:15 +0700

    merge catalog (#7)
    * update unit test for grid card and ca
    
    * update search code
    
    * update redeem
    
    * Upload html/css redeem page and search block
    
    * upload search.scss
    
    * update search bar
    
    * edit wrong font
    
    * edit positiion redeem block
    
    * filter course by term in search bar
    
    * Edit the result block overlaps search bar
    
    * remove debug and comment code
    
    * import lodash
    
    * update search by id and creator
    
    * update css for stylelint
    
    * revert code carousel (transition ver 1)
    
    * fix compile error

--- src/stores/Catalog.js
+++ src/stores/Catalog.js
@@ -1,6 +1,11 @@
 import EventEmitter from 'events';
 
+import AppDispatcher from 'nti-lib-dispatcher';
 import {getLink} from 'nti-lib-interfaces';
+
+import * as Constants from '../Constants';
+
+
 
 const CHANGE = 'change';
 
@@ -12,22 +17,37 @@ export default class CatalogStore extends EventEmitter {
 
 		this.service = service;
 		this.href = href;
+		AppDispatcher.register ((payload) => {
+			const action = payload.action;
+			switch (action.type) {
+			case Constants.VIEW_ALL_COURSES:
+				this.search = {
+					searching: false
+				};
+				this.emit (CHANGE, {type: 'search'});
+				break;
+			}
+		});
 	}
 
 
 	async load () {
 		const {Items: courses, Links: links} = await this.service.get (this.href);
-		const {Items: popular} = await this.service.get (getLink(links, 'popular'));
-		const {Items: carousel} = await this.service.get (getLink(links, 'Featured'));
+		const {Items: popular} = await this.service.get (getLink (links, 'popular'));
+		const {Items: carousel} = await this.service.get (getLink (links, 'Featured'));
 
-		const parse = x => this.service.getObject(x);
-		this.courses = await Promise.all( courses.map(parse) );
-		this.popular = await Promise.all( popular.map(parse) );
-		this.carousel = await Promise.all( carousel.map(parse) );
+		const parse = x => this.service.getObject (x);
+		this.courses = await Promise.all (courses.map (parse));
+		this.popular = await Promise.all (popular.map (parse));
+		this.carousel = await Promise.all (carousel.map (parse));
+		this.search = {
+			searching: false
+		};
 
 		this.emit (CHANGE, {type: 'courses'});
 		this.emit (CHANGE, {type: 'popular'});
 		this.emit (CHANGE, {type: 'carousel'});
+		this.emit (CHANGE, {type: 'search'});
 	}
 
 
@@ -35,6 +55,14 @@ export default class CatalogStore extends EventEmitter {
 		return this[key];
 	}
 
+	updateSearchTerm (term) {
+		this.search = {
+			searching: true,
+			term: term,
+			courses: this.courses
+		};
+		this.emit ('change', {type: 'search'});
+	}
 
 	addChangeListener (fn) {
 		this.addListener (CHANGE, fn);
