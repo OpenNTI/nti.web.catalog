819166cb401647a601145b240e00bc1d2a559aa9
Son Nguyen <sonnguyen@goappable.com>
2017-11-08 18:39:25 +0700

    format code for build

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -30,8 +30,8 @@ export default class Carousel extends React.Component {
 		}
 	}
 
-	componentWillMount(){
-		clearInterval(this.intervalID);
+	componentWillMount () {
+		clearInterval (this.intervalID);
 		if (this.props.data) {
 			this.intervalID = setInterval (this.nextSlide, slideShowDelay);
 		}
