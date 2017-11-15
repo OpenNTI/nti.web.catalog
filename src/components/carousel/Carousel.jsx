fe66e0db399ef14c304512bfdec32a0e8ba25c01
Son Nguyen <sonnguyen@goappable.com>
2017-11-15 14:29:32 +0700

    NTI-4374: remove the auto-swipe timer in carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -31,10 +31,10 @@ export default class Carousel extends React.Component {
 	}
 
 	componentWillMount () {
-		clearInterval (this.intervalID);
-		if (this.props.data) {
-			this.intervalID = setInterval (this.nextSlide, slideShowDelay);
-		}
+		// clearInterval (this.intervalID);
+		// if (this.props.data) {
+		// 	this.intervalID = setInterval (this.nextSlide, slideShowDelay);
+		// }
 	}
 	componentWillUnmount () {
 		clearInterval(this.intervalID);
