3b19ca03e2e2bb27d383f6528e723bd68a05937d
Son Nguyen <sonnguyen@goappable.com>
2017-11-16 10:55:00 +0700

    NTI-4429: description is long

--- src/components/carousel/carousel-item/__test__/CarouselItem.spec.js
+++ src/components/carousel/carousel-item/__test__/CarouselItem.spec.js
@@ -8,6 +8,7 @@ describe('Carousel', () => {
 	const data = {
 		NTIID: 'tag:nextthought.com,2011-10:NTI-CourseInfo-Alpha_NTI_1010',
 		title: 'Preview Skeleton Test Course',
+		description:'description'
 	};
 	test('Course title is string', () => {
 		const wrapper = shallow(<CarouselItem data={data} />);
