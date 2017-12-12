85fc10de83247df805202cf728589d64d4ee2402
Son Nguyen <sonnguyen@goappable.com>
2017-12-12 11:34:00 +0700

    unit test for CarouselItem

--- src/carousel/carousel-item/__test__/CarouselItem.spec.js
+++ src/carousel/carousel-item/__test__/CarouselItem.spec.js
@@ -5,13 +5,34 @@ import {shallow} from 'enzyme';
 import CarouselItem from '../components/CarouselItem';
 
 describe('Carousel', () => {
+	const ntiid = 'tag:nextthought.com,2011-10:NTI-CourseInfo-Alpha_NTI_1010';
+	const title = 'Preview Skeleton Test Course';
+	const richDescription = 'RichDescription';
+	const description = 'description';
 	const data = {
-		NTIID: 'tag:nextthought.com,2011-10:NTI-CourseInfo-Alpha_NTI_1010',
-		title: 'Preview Skeleton Test Course',
-		description:'description'
+		NTIID: ntiid,
+		title: title,
+		RichDescription: richDescription,
 	};
+
+	const dataTemp = {
+		NTIID: ntiid,
+		title: title,
+		description: description
+	};
+
 	test('Course title is string', () => {
 		const wrapper = shallow(<CarouselItem data={data} />);
-		expect(wrapper.find('data').title === 'Title');
+		expect(wrapper.find('.title-carousel').first().text()).toEqual(title);
+	});
+
+	test('Course description when RichDescription define', () => {
+		const wrapper = shallow(<CarouselItem data={data} />);
+		expect(wrapper.find('.detail-txt').first().text()).toEqual(richDescription);
+	});
+
+	test('Course description when RichDescription undefined', () => {
+		const wrapper = shallow(<CarouselItem data={dataTemp} />);
+		expect(wrapper.find('.detail-txt').first().text()).toEqual(description);
 	});
 });
