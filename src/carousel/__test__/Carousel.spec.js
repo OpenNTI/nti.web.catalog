10acc08d91daa818b9f0e463a3672a92a520bb1c
Son Nguyen <sonnguyen@goappable.com>
2017-12-12 11:36:59 +0700

    unit test fro Carousel

---
+++ src/carousel/__test__/Carousel.spec.js
@@ -0,0 +1,74 @@
+/* eslint-env jest */
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Carousel from '../components/Carousel';
+
+describe('Carousel', () => {
+	const selectCarousel = jest.fn();
+	const index = 0;
+	const data = [
+		{
+			NTIID: 'tag:nextthought.com,2011-10:NTI-CourseInfo-Alpha_NTI_1010'
+		},
+		{
+			NTIID: 'tag:nextthought.com,2011-10:NTI-CourseInfo-Alpha_NTI_1010'
+		},
+		{
+			NTIID: 'tag:nextthought.com,2011-10:NTI-CourseInfo-Alpha_NTI_1010'
+		}
+	];
+
+	const getCmp = () => mount(
+		<Carousel
+			data={data}
+			carouselSelected={index}
+			selectCarousel={selectCarousel}
+		/>
+	);
+
+	test('Test click next carousel', (done) => {
+		const cmp = getCmp();
+
+		const closeButton = cmp.find('.icon-chevronup-25').first();
+
+		closeButton.simulate('click');
+
+		const verifyCalled = () => {
+			expect(selectCarousel).toHaveBeenCalled();
+
+			done();
+		};
+		verifyCalled();
+	});
+
+	test('Test click pre carousel', (done) => {
+		const cmp = getCmp();
+
+		const closeButton = cmp.find('.icon-chevrondown-25').first();
+
+		closeButton.simulate('click');
+
+		const verifyCalled = () => {
+			expect(selectCarousel).toHaveBeenCalled();
+
+			done();
+		};
+
+		verifyCalled();
+	});
+
+	test('Test change carousel selected index when click next', () => {
+		const cmp = getCmp();
+		expect(cmp.state().selectedIndex).toEqual(0);
+		cmp.find('.icon-chevronup-25').simulate('click');
+		expect(cmp.state().selectedIndex).toEqual(1);
+	});
+
+	test('Test change carousel selected index when click previous', () => {
+		const cmp = getCmp();
+		expect(cmp.state().selectedIndex).toEqual(0);
+		cmp.find('.icon-chevrondown-25').simulate('click');
+		expect(cmp.state().selectedIndex).toEqual(data.length - 1);
+	});
+});
