f73354247b8c236173fa372be1e1dc9f284cb07a
Son Nguyen <sonnguyen@goappable.com>
2017-12-13 14:59:50 +0700

    unit test for Grid card

--- src/grid-card/__test__/GridCard.spec.js
+++ src/grid-card/__test__/GridCard.spec.js
@@ -1,12 +1,33 @@
 /* eslint-env jest */
+import React from 'react';
+import { mount } from 'enzyme';
 
-//example test:
-describe('Unit Test Example', () => {
+import GridCard from '../components/GridCard';
+import * as Constants from '../../Constants';
 
-	test('should show a test', () => {
-
-		expect(true).toBeTruthy();
-
+describe('GridCard', () => {
+	const purchasedCourses = [
+		{
+			NTIID: 'tag:nextthought.com,2011-10:NTI-CourseInfo-Alpha_NTI_1010',
+			getDefaultAssetRoot: ()=>{}
+		},
+		{
+			NTIID: 'tag:nextthought.com,2011-10:NTI-CourseInfo-Alpha_NTI_1010',
+			getDefaultAssetRoot: ()=>{}
+		},
+		{
+			NTIID: 'tag:nextthought.com,2011-10:NTI-CourseInfo-Alpha_NTI_1010',
+			getDefaultAssetRoot: ()=>{}
+		}
+	];
+	const getCmpWithPuchased = () => mount(
+		<GridCard
+			courses={purchasedCourses}
+			type={Constants.PURCHASED}
+		/>
+	);
+	test('Test grid card with purchased tag, count courses', () => {
+		const cmp = getCmpWithPuchased();
+		expect(cmp.find('li').length).toEqual(purchasedCourses.length);
 	});
-
 });
