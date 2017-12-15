d8af302ecdc7485d1f352f24e32d22f61ec33da6
Son Nguyen <sonnguyen@goappable.com>
2017-12-15 13:27:41 +0700

    update unit test for redeem

--- src/redeem/__test__/Redeem.spec.js
+++ src/redeem/__test__/Redeem.spec.js
@@ -1,11 +1,35 @@
 /* eslint-env jest */
 import React from 'react';
-import { mount } from 'enzyme';
+import {mount} from 'enzyme';
 
 import Redeem from '../components/View';
 
 const mockService = () => ({
-	getCollection: (o) => Promise.resolve(o)
+	getCollection: (o) => {
+		return Promise.resolve({
+			Links: [{rel: 'accept-course-invitations'}]
+		});
+	},
+	post: (link, code) => {
+		if (code.invitation_codes === 'Success') {
+			return Promise.resolve(
+				{
+					CatalogEntryNTIID: 'tag:nextthought.com,2011-10:NTI-CourseInfo-Alpha_NTI_1010',
+					getDefaultAssetRoot: ()=>{}
+				}
+			);
+		}
+		else if (code.invitation_codes === 'Enrolled') {
+			return Promise.reject({
+				message: "You are already enrolled in this course."
+			});
+		}
+		else {
+			return Promise.reject({
+				message: "Invalid invitation code."
+			});
+		}
+	}
 });
 
 const onBefore = () => {
@@ -25,9 +49,11 @@ const onAfter = () => {
 describe('Redeem', () => {
 	const errorMessage = {
 		empty: 'Could not redeem course code',
+		invalid: 'Invalid invitation code.',
+		enrolled: 'You are already enrolled in this course.'
 	};
 
-	const match = {params:{}};
+	const match = {params: {}};
 
 	const getCmp = () => mount(
 		<Redeem match={match}/>
@@ -38,8 +64,61 @@ describe('Redeem', () => {
 
 	test('Test redeem with empty code', () => {
 		const cmp = getCmp();
-		cmp.setState({codeValue:''});
+		cmp.setState({codeValue: ''});
 		cmp.find('button').simulate('click');
 		expect(cmp.find('.error-redeem p').first().text()).toEqual(errorMessage.empty);
+	});
+
+	test('Test redeem with invalid code', async () => {
+		const cmp = getCmp();
+		cmp.setState({codeValue: 'Invalid'});
+		cmp.find('button').simulate('click');
+		await new Promise(resolve => {
+			setTimeout(() => {
+				resolve();
+			}, 1000);
+		});
+
+		expect(cmp.state().errorMessage).toEqual(errorMessage.invalid);
+	});
+
+	test('Test redeem with enrolled code', async () => {
+		const cmp = getCmp();
+		cmp.setState({codeValue: 'Enrolled'});
+		cmp.find('button').simulate('click');
+		await new Promise(resolve => {
+			setTimeout(() => {
+				resolve();
+			}, 1000);
+		});
+
+		expect(cmp.state().errorMessage).toEqual(errorMessage.enrolled);
+	});
+
+	test('Test redeem success', async () => {
+		const cmp = getCmp();
+		cmp.setState({codeValue: 'Success'});
+		cmp.find('button').simulate('click');
+		await new Promise(resolve => {
+			setTimeout(() => {
+				resolve();
+			}, 300);
+		});
+
+		expect(cmp.state().error).toEqual(false);
+	});
+
+	test('Test redeem loading', async () => {
+		const cmp = getCmp();
+		cmp.setState({codeValue: 'Success'});
+		cmp.find('button').simulate('click');
+		expect(cmp.state().loading).toEqual(true);
+		await new Promise(resolve => {
+			setTimeout(() => {
+				resolve();
+			}, 300);
+		});
+
+		expect(cmp.state().loading).toEqual(false);
 	});
 });
