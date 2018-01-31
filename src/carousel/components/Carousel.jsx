98d0df1bd0a87432d9a41b6b7d8bedd8ac941b01
Jonathan Grimes <jsg2021@gmail.com>
2018-01-31 14:28:24 -0600

    upgrade react-transition-group

--- src/carousel/components/Carousel.jsx
+++ src/carousel/components/Carousel.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import PropTypes from 'prop-types';
 
 import CarouselItem from '../carousel-item/components/CarouselItem';
 
-const transitionEnterTimeout = 1000;
-const transitionLeaveTimeout = 1000;
+const transitionTimeout = 1000;
+
 export default class Carousel extends React.Component {
 
 	static propTypes = {
@@ -30,50 +30,73 @@ export default class Carousel extends React.Component {
 			this.setState({
 				slideLock: false
 			});
-		}, transitionEnterTimeout);
+		}, transitionTimeout);
 	}
 
 	nextSlide = () => {
-		if (this.state.slideLock) {
+		const {
+			props: {data, selectCarousel},
+			state: {slideLock, selectedIndex}
+		} = this;
+
+		if (slideLock) {
 			return;
 		}
-		const nextIndex = (this.state.selectedIndex < this.props.data.length - 1) ? this.state.selectedIndex + 1 : 0;
-		this.props.selectCarousel(nextIndex);
+
+		const nextIndex = (selectedIndex < data.length - 1) ? selectedIndex + 1 : 0;
+
+		selectCarousel(nextIndex);
+
 		this.setState ({selectedIndex: nextIndex, transitionStyle: 'next', slideLock: true}, () => {
-			this.unlockSlide ();
+			this.unlockSlide();
 		});
 
 	}
 
 	preSlide = () => {
-		if (this.state.slideLock) {
+		const {
+			props: {data, selectCarousel},
+			state: {slideLock, selectedIndex}
+		} = this;
+
+		if (slideLock) {
 			return;
 		}
-		const preIndex = (this.state.selectedIndex > 0) ? this.state.selectedIndex - 1 : this.props.data.length - 1;
-		this.props.selectCarousel(preIndex);
+
+		const preIndex = (selectedIndex > 0) ? selectedIndex - 1 : data.length - 1;
+
+		selectCarousel(preIndex);
+
 		this.setState ({selectedIndex: preIndex, transitionStyle: 'previous', slideLock: true}, () => {
-			this.unlockSlide ();
+			this.unlockSlide();
 		});
 	}
 
 	render () {
-		if (!this.props.data || this.props.data.length === 0) {
+		const {
+			props: {
+				data
+			},
+			state: {
+				selectedIndex,
+				transitionStyle,
+			}
+		} = this;
+
+		if (!data || data.length === 0) {
 			return null;
 		}
 
-		const carouselItemList = this.props.data.map (function (item, index) {
-			return (
-				<CarouselItem data={item} key={index}/>
-			);
-		});
 		return (
 			<div className="carousel-content--image">
-				<CSSTransitionGroup
-					transitionName={'animation--' + this.state.transitionStyle}
-					transitionEnterTimeout={transitionEnterTimeout}
-					transitionLeaveTimeout={transitionLeaveTimeout}>
-					{carouselItemList[this.state.selectedIndex]}
-				</CSSTransitionGroup>
+				<TransitionGroup>
+					<CSSTransition key={selectedIndex}
+						classNames={'animation--' + transitionStyle}
+						timeout={transitionTimeout}
+					>
+						<CarouselItem data={data[selectedIndex]} />
+					</CSSTransition>
+				</TransitionGroup>
 				<button className="icon-chevronup-25" onClick={this.nextSlide}/>
 				<button className="icon-chevrondown-25" onClick={this.preSlide}/>
 			</div>
