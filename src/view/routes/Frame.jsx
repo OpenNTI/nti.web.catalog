5a18d99016f4d6ded13e9298080385bd271fb2ad
Ray Hatfield <ray.hatfield@gmail.com>
2021-05-19 09:40:07 -0500

    move getRouteFor into Routes.js

--- src/view/routes/Frame.jsx
+++ src/view/routes/Frame.jsx
@@ -29,12 +29,6 @@ function isActiveRoute(route, pathname, baseroute) {
 	return pathname.indexOf(route) === 0;
 }
 
-const getRouteFor = obj => {
-	if ((obj?.isCourseCatalogEntry || obj?.isCourse) && obj.getID?.()) {
-		return `./nti-course-catalog-entry/${obj.getID()}`;
-	}
-};
-
 CatalogFrame.propTypes = {
 	children: PropTypes.any,
 };
@@ -71,11 +65,7 @@ export default function CatalogFrame({ children }) {
 					active={redeemActive}
 				/>
 			</Navigation.Tabs>
-			<Router.RouteForProvider getRouteFor={getRouteFor}>
-				<div className={cx('catalog-frame', 'course-catalog')}>
-					{child}
-				</div>
-			</Router.RouteForProvider>
+			<div className={cx('catalog-frame', 'course-catalog')}>{child}</div>
 		</>
 	);
 }
--- src/view/routes/available/Routes.js
+++ src/view/routes/available/Routes.js
@@ -8,6 +8,11 @@ export default Router.for([
 	Route({
 		path: ['/', '/nti-course-catalog-entry/:entryId'],
 		component: View,
+		getRouteFor: obj => {
+			if ((obj?.isCourseCatalogEntry || obj?.isCourse) && obj.getID?.()) {
+				return `./nti-course-catalog-entry/${obj.getID()}`;
+			}
+		},
 		exact: true,
 	}),
 	Route({
