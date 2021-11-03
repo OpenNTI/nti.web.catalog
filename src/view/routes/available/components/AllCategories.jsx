855f1a69769b8174966173015d07493e91b6f893
Jonathan Grimes <jsg2021@gmail.com>
2021-11-03 12:03:29 -0500

    NTI-11394 - Get the empty state to show

--- src/view/routes/available/components/AllCategories.jsx
+++ src/view/routes/available/components/AllCategories.jsx
@@ -1,8 +1,8 @@
+import { Suspense } from 'react';
 
-import { getService } from '@nti/web-client';
-import { Hooks, Loading, Layouts, Text } from '@nti/web-commons';
+import { Loading, Layouts, Text } from '@nti/web-commons';
+import { ErrorBoundary, useAsyncValue, useService } from '@nti/web-core';
 import { LinkTo } from '@nti/web-routing';
-import { url } from '@nti/lib-commons';
 
 import { NTIOtherCategory, BatchSize } from '../../../Constants';
 import Carousel from '../../../components/carousel';
@@ -11,8 +11,6 @@ import Category from '../../../components/category';
 import PageError from '../../../components/PageError';
 
 const { Responsive } = Layouts;
-const { useResolver } = Hooks;
-const { isPending, isResolved, isErrored } = useResolver;
 
 const BottomMenu = styled(Text.Base).attrs({ as: 'section' })`
 	display: block;
@@ -20,85 +18,99 @@ const BottomMenu = styled(Text.Base).attrs({ as: 'section' })`
 	padding-bottom: 1rem;
 `;
 
-async function loadFeatured(catalog, service) {
-	try {
-		const featured = await service.getBatch(catalog.getLink('Featured'));
+export default function AllCategories() {
+	return (
+		<ErrorBoundary fallback={<PageError />}>
+			<>
+				<Suspense fallback={<Loading.Spinner.Large />}>
+					<Content />
+				</Suspense>
+				{Responsive.isMobileContext() && (
+					<BottomMenu>
+						<div>
+							<LinkTo.Name name="catalog.redeem">
+								Redeem a Course
+							</LinkTo.Name>
+						</div>
+						<LinkTo.Name name="contact-us">Contact us</LinkTo.Name>
+					</BottomMenu>
+				)}
+			</>
+		</ErrorBoundary>
+	);
+}
 
-		return featured.Items;
-	} catch (e) {
+function Content() {
+	const { featured, items, other } = useCatalog();
+	return (
+		<>
+			<Carousel featured={featured} />
+			{other ? (
+				<Category category={other} header={false} />
+			) : (
+				<Categories categories={items} />
+			)}
+		</>
+	);
+}
+
+function useCatalog() {
+	const service = useService();
+	const catalog = service.getCollection('Courses', 'Catalog');
+
+	return useAsyncValue(`anonymous - ${catalog.href}`, async () => {
+		const byTag = `ByTag/${NTIOtherCategory}`;
+
+		const [featured, tags] = await Promise.all([
+			loadFeatured(catalog),
+			loadSuggestedTags(catalog),
+		]);
+
+		if (!tags?.Items?.length) {
+			const other = await catalog.fetchLink({
+				rel: byTag,
+				mode: 'batch',
+				params: { batchSize: BatchSize, batchStart: 0 },
+			});
+
+			return {
+				featured,
+				other: !other.total ? null : other,
+			};
+		}
+
+		const others = await catalog.fetchLink({
+			rel: byTag,
+			mode: 'batch',
+			params: { batchSize: 4, batchStart: 0 },
+		});
+
+		const otherTag = {
+			tag: NTIOtherCategory,
+			count: others.total,
+			Items: others.Items,
+		};
+
+		return {
+			featured,
+			items: others.Total > 0 ? [...tags.Items, otherTag] : tags.Items,
+		};
+	});
+}
+
+async function loadFeatured(catalog) {
+	try {
+		return (await catalog.fetchLink({ rel: 'Featured', mode: 'batch' }))
+			.Items;
+	} catch {
 		return [];
 	}
 }
 
-export default function AllCategories() {
-	const resolver = useResolver(async () => {
-		const service = await getService();
-		const catalog = service.getCollection('Courses', 'Catalog');
-		const byTag = catalog.getLink('ByTag');
-
-		const suggestedTags = catalog.hasLink('SuggestedTags')
-			? catalog.fetchLink({ mode: 'raw', rel: 'SuggestedTags' })
-			: { Items: [] };
-
-		const [featured, tags] = await Promise.all([
-			loadFeatured(catalog, service),
-			suggestedTags,
-		]);
-
-		if (tags.Items.length === 0) {
-			const otherBucket = await service.getBatch(
-				url.join(byTag, NTIOtherCategory),
-				{ batchSize: BatchSize, batchStart: 0 }
-			);
-
-			return { featured, other: otherBucket, onlyOther: true };
-		} else {
-			const others = await service.getBatch(
-				url.join(byTag, NTIOtherCategory),
-				{ batchSize: 4, batchStart: 0 }
-			);
-			const otherTag = {
-				tag: NTIOtherCategory,
-				count: others.Total,
-				Items: others.Items,
-			};
-
-			return {
-				featured,
-				Items:
-					others.Total > 0 ? [...tags.Items, otherTag] : tags.Items,
-				onlyOther: false,
-			};
-		}
-	}, []);
-
-	const loading = isPending(resolver);
-	const error = isErrored(resolver) ? resolver : null;
-	const categories = !error && isResolved(resolver) ? resolver : null;
-
-	const items = categories?.Items;
-	const featured = categories?.featured;
-	const other = categories?.other;
-
-	return (
-		<Loading.Placeholder
-			loading={loading}
-			fallback={<Loading.Spinner.Large />}
-		>
-			{error && <PageError error={error} />}
-			{featured && <Carousel featured={featured} />}
-			{other && <Category category={other} header={false} />}
-			{items && !other && <Categories categories={items} />}
-			{Responsive.isMobileContext() && (
-				<BottomMenu>
-					<div>
-						<LinkTo.Name name="catalog.redeem">
-							Redeem a Course
-						</LinkTo.Name>
-					</div>
-					<LinkTo.Name name="contact-us">Contact us</LinkTo.Name>
-				</BottomMenu>
-			)}
-		</Loading.Placeholder>
-	);
+async function loadSuggestedTags(catalog) {
+	try {
+		return await catalog.fetchLink({ mode: 'raw', rel: 'SuggestedTags' });
+	} catch {
+		return { Items: [] };
+	}
 }
